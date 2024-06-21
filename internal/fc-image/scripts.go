f89fb5410296baa4cfdafd2157d61cc6812c450d
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-06-21 10:04:17 -0600

    slightly better copy function
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- internal/fc-image/scripts.go
+++ internal/fc-image/scripts.go
@@ -4,7 +4,7 @@ const (
 	copy_fs = `#!/bin/sh
 set -xe
 
-for d in bin boot etc lib root sbin usr var run; do tar c "/$d" | tar x -C /tmp/rootfs; done
+ls / | grep -v dev | grep -v proc | grep -v sys | grep -v tmp | while read d; do tar c "/$d" | tar x -C /tmp/rootfs; done
 for dir in dev proc sys tmp; do mkdir /tmp/rootfs/${dir}; done
 
 chmod 1777 /tmp/rootfs/tmp
